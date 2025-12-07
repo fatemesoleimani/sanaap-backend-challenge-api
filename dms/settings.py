f51841dd8b469c8b88936c55cbaf78d945253bb9
fateme <fateme.soleimani.d@gmail.com>
2025-12-07 22:08:41 +0330

    update document view

--- dms/settings.py
+++ dms/settings.py
@@ -153,15 +153,12 @@ DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = config("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_ENDPOINT_URL = config("AWS_S3_ENDPOINT_URL")
-
-AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
-AWS_QUERYSTRING_AUTH = True
-AWS_S3_REGION_NAME = "us-east-1"  # can be anything for MinIO
-AWS_DEFAULT_ACL = None
-AWS_S3_FILE_OVERWRITE = False
-
-
+AWS_S3_ENDPOINT_URL = config("AWS_S3_ENDPOINT_URL")  # http://minio_storage:9000
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_ADDRESSING_STYLE = "path"  # Important for MinIO
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
